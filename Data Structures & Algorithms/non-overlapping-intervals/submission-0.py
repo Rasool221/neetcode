e6f79b6a241feadebf6c1b78f10130f322e46046
Rasool A <rasoolabbas017@gmail.com>
2026-05-13 20:55:33 -0500

    Add: non-overlapping-intervals - submission-0

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-0.py
@@ -0,0 +1,22 @@
+# first we sort by the first element of the interval
+# then we simply count the amount of overlapping ones
+# well that wont return the minimum always, so we can revisit that later, but 
+# i want to get the shell of the solution set up first
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # sort by first element so we can count by looking
+        # ahead
+        intervals.sort(key=lambda x: x[0]) 
+
+        amt_to_remove = 0
+        
+        # count overlapping intervals
+        # we dont need to look at the last element
+        for i in range(len(intervals) - 1): 
+            _, b = intervals[i]
+            c, d = intervals[i + 1]
+
+            if b > c and b < d:
+                amt_to_remove += 1
+            
+        return amt_to_remove
