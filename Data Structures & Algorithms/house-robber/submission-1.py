14ae30bda7c9894e0fa164aeb1e4064e7739f526
Rasool A <rasoolabbas017@gmail.com>
2026-05-15 18:10:35 -0500

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,25 @@
+# standard dp problem, we can do a bottoms up approach where
+# for every item, we would look at i-2 back and add with it robbing 2 houses ago or not robbing 2 houses ago
+# if that item doesnt have a house i-2 away, we are forced to keep it as it is 
+# then we keep going for every house
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(len(nums)):
+            prev = 0
+            if i > 1:
+                prev = nums[i - 2]
+
+            nums[i] = max(nums[i], prev + nums[i])
+
+        return max(nums[-1], nums[-2])
+        
+            
