91a4bd2e2792cd77505966b736606fc04dacb9e0
Rasool A <rasoolabbas017@gmail.com>
2026-07-16 16:45:28 -0500

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        g = {}
+
+        for f, t in tickets:
+            if f not in g:
+                g[f] = []
+
+            g[f].append(t)
+            g[f].sort(reverse=True) # so we can .pop() in O(1)
+
+        answer = []
+
+        def dfs(fr: str):
+            while g[fr]:
+                dfs(g[fr].pop())
+
+            answer.append(fr)
+
+        dfs("JFK")
+        return answer[::-1]
+
+
+
